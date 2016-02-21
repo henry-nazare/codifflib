8399770b1f824e4ce00d233112e02aef0191dc44
Henrique <henrique.nazare.santos@gmail.com>
2016-02-21 12:06:47 -0300

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='codifflib',
+    version='0.1',
+    description='Syntax highlighted code diffs',
+    author='Henrique Nazare',
+    author_email='henrique.nazare.santos@gmail.com',
+    url='https://github.com/nzre/codifflib',
+    packages=['codifflib'],
+)
+
